61ce30c12713cf00e09c2abc2d05c8bd730c6ea5
Dan Calacci <dan@dcalacci.net>
2013-10-27 14:39:32 -0400

    Sentence class is looking more developed.
    should be able to streamline the classifier code now

--- scripts/classifier.py
+++ scripts/classifier.py
@@ -6,23 +6,63 @@ import liwc.liwcUtils as liwcUtils
 liwc = liwcUtils.LiwcDict()
 
 class Sentence:
-    def __init__(self, sen=""):
-        self.text = sen
-        self.sen = self.__prep_text()
+    """
+    Sentence is a class that represents a single speech act.
+    It includes utilities for extracting all entities in the speech act
+    using the stanford NER package, and producing a list representation
+    of a speech act where entities are individual list entries.
 
-    def __prep_text(self):
-        sen = self.text.replace(".", "")
-        sen = self.text.replace(",", "")
-        sen = self.text.lower().split()
+    Creating a new Sentence instance should be all you need - you can
+    then access the new representation of the sentence by accessing the
+    'sen' member.
+    """
+    def __init__(self, sen=""):
+        self.orig_text = sen
+        self.text = self.__prep_text(self.orig_text)
+        self.entities = self.get_entities()
+        self.sen = self.parse_sentence_with_entities(self.entities)
+
+    def __prep_text(self, text):
+        """
+        Removes all periods and commas from a given string,
+        and lowercases all the words in the sentence.
+        """
+        sen = text.replace(".", "")
+        sen = sen.replace(",", "")
+        sen = sen.lower()
         return sen
 
     def get_entities(self):
-        "produces a dict of entities in this sentence"
+        """
+        Generates the collections of entities for this sentence.
+        populates 'self.original_entities' with the output from
+        the stanford ner server. This dictionary is of the form:
+        TYPE -> [Listof Entities]
+        Also returns a list of all the entities stanford NER recognizes
+        in this sentence, as a list of strings.
+        """
         import ner
         tagger = ner.SocketNER(host='localhost', port=8080)
-        return tagger.get_entities(self.text)
+        orig_ents = tagger.get_entities(self.orig_text)
 
-    def parse_entities(self, sen, entities):
+        for key, val in orig_ents.items():
+            orig_ents[key] = map(lambda s: self.__prep_text(s), val)
+        self.original_entities = orig_ents
+
+        # just a simple list of all the entities
+        entities = [v[0] for k, v in orig_ents.items()]
+        return entities
+
+    def __parse_entities(self, sen, entities):
+        """
+        produces a list representation of the given sentence with each entity
+        as its' own list element. Example:
+        'i hate northesatern university, but i like john adams'
+        ->
+        ['i', 'hate', 'northeastern university', 'but', 'i', 'like', 'john adams']
+        this will apply this transformation to multiple references to the same 
+        entity, as well.
+        """
         # if we're done going through the list of entities,
         # just return the sentence array.
         if not entities:
@@ -31,7 +71,7 @@ class Sentence:
         entity = entities[0]
         # if it's not in the list, go to the next entity.
         if sen.find(entity) == -1:
-            return self.parse_entities(sen, entities[1:])
+            return self.__parse_entities(sen, entities[1:])
 
         beg_index = sen.find(entity)
         end_index = sen.find(entity) + len(entity)
@@ -39,11 +79,16 @@ class Sentence:
         a = sen[:beg_index] # string up to the entity
         b = sen[end_index:] # string after the entity
 
-        a = self.parse_entities(a, entities)
+        a = self.__parse_entities(a, entities)
         a.append(entity) # add the entity inbetween the two parts
-        b = self.parse_entities(b, entities)
+        b = self.__parse_entities(b, entities)
 
         return a + b
+
+    def parse_sentence_with_entities(self, entities):
+        "initial call to __parse_entities"
+        return self.__parse_entities(self.text, self.entities)
+
 
 
 def __isNegated(sen, word):
