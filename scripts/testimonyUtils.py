0fb7bf97a41d99a020bb31116c9132adbf55779b
Dan Calacci <dan@dcalacci.net>
2013-11-04 00:43:51 -0500

    testimonyUtils now guesses the correct names for speakers
    Using levenshtein distance and a distribution of speaker names in the
    transcript, it guesses the most likely name for some particular speaker
    name found through regular expressions

--- scripts/testimonyUtils.py
+++ scripts/testimonyUtils.py
@@ -31,6 +31,44 @@ def cleanFile(filepath):
 
 def get_speech_acts(filepath):
     "returns a hash of name -> list of speech acts"
+
+    # some functions to guess likely names from mispellings. works OK
+    def are_close(a, b):
+        "returns true if the levenshtein ratio between a and b is greater than 0.6"
+        import Levenshtein
+        ratio = Levenshtein.ratio(a, b)
+        return ratio > 0.6
+
+    def has_similar(s, names):
+        "returns a name of a similar name if it exists. None otherwise."
+        for name in names:
+            if are_close(name, s):
+                return name.lower()
+        return None
+
+    def name_distribution(matches):
+        "computes a dict of name->% for all speech acts"
+        import collections
+        dist = collections.defaultdict(lambda: 0)
+        
+        for match in matches:
+            dist[match[0]] += 1
+
+            # prefer names in all caps
+            if match[0].isupper():
+                dist[match[0]] += 10
+        l = len(dist.keys())
+        for key in dist.keys():
+            dist[key] = dist[key]/float(l)
+            
+        return dist
+        
+    def find_likely_name(name, dist):
+        "returns the most likely name given the a distribution of names"
+        maybes = dict((k, v) for k, v in dist.items() if are_close(name.lower(), k.lower()))
+        best = max(maybes.items(), key=lambda p: p[1])
+        return best[0]
+        
     cleanFile(filepath)
     str = ""
     with open(filepath, 'r+') as f:
@@ -40,12 +78,16 @@ def get_speech_acts(filepath):
 
     regex = re.compile("^(?:Mrs|Miss|Mr)(?:\.?)(?:\s?)(\w*?)[\.\s](.*?)\n",re.MULTILINE)
     matches = regex.findall(str)
+
+    # name distribution to guess likely names from mispellings
+    dist = name_distribution(matches)
     speechacts = {}
     for match in matches:
-        if speechacts.has_key(match[0].lower()):
-            speechacts[match[0].lower()].append(match[1])
+        likely_name = find_likely_name(match[0], dist).lower()
+        if speechacts.has_key(likely_name):
+            speechacts[likely_name].append(match[1])
         else:
-            speechacts[match[0].lower()] = [match[1]]
+            speechacts[likely_name] = [match[1]]
     return speechacts
 
 def splitFileByTestimony(filepath):
