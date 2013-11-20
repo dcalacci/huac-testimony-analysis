d918e69f778c06f150a36128b3be53772d83b9f1
Dan Calacci <dan@dcalacci.net>
2013-11-20 15:31:56 -0500

    don't wanna commit all that text

--- scripts/other/ner.rb
+++ scripts/other/ner.rb
@@ -6,5 +6,6 @@ end
 
 Dir[File.join(path, "*.txt")].each do |file|
   new = File.basename(file, File.extname(file))
-  system("/Users/dan/attic/tools/stanford-ner-2013-06-20/ner #{file} > tmp/#{new}.ner")
+  system("/Users/dan/attic/tools/nlp/ner/stanford-ner-2013-06-20/ner #{file} > tmp/#{new}.ner")
+#  system("/Users/dan/attic/tools/stanford-ner-2013-06-20/ner #{file} > tmp/#{new}.ner")
 end
