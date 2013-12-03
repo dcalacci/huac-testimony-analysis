2039e8582f32eb696f4377f5c3d16f6f47bede19
Dan Calacci <dan@dcalacci.net>
2013-12-03 12:47:32 -0500

    distribution stuff somewhat completed, named lists
    pickles contain graphs for each year of hearings.
    1952 and 1953 are really the only ones with good 'named'
    lists.

--- testimony/namedist.py
+++ testimony/namedist.py
@@ -47,7 +47,7 @@ def name_distribution_from_dict(d):
     the distribution by looking at the number of characters 
     attributed to particular names in the speechact dict. The more
     characters a name has associated with it, the higher the
-    distribution for that name.
+    score for that name.
     """
     def get_number_chars(los):
         "returns the number of characters in the given list of strings"
@@ -70,3 +70,33 @@ def find_likely_name(name, dist):
                                                                k.lower()))
     best = max(maybes.items(), key=lambda p: p[1])
     return best[0]
+
+
+# use this to find the distance from one name to another.
+def fuzzy_substring(needle, haystack):
+    "taken from: http://ginstrom.com/scribbles/2007/12/01/fuzzy-substring-matching-with-levenshtein-distance-in-python/"
+    """Calculates the fuzzy match of needle in haystack,
+    using a modified version of the Levenshtein distance
+    algorithm.
+    The function is modified from the levenshtein function
+    in the bktree module by Adam Hupp"""
+    m, n = len(needle), len(haystack)
+
+    # base cases
+    if m == 1:
+        return not needle in haystack
+    if not n:
+        return m
+
+    row1 = [0] * (n+1)
+    for i in range(0,m):
+        row2 = [i+1]
+        for j in range(0,n):
+            cost = ( needle[i] != haystack[j] )
+
+            row2.append( min(row1[j+1]+1, # deletion
+                               row2[j]+1, #insertion
+                               row1[j]+cost) #substitution
+                           )
+        row1 = row2
+    return min(row1)
--- testimony/parse-names.py
+++ testimony/parse-names.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 import re
-import os
 import time
 import ner
-import subprocess
+import namedist
+from collections import defaultdict
 
+#d2 = dict((k, map(lambda n: " ".join(n.split()), v)) for (k,v) in d2.items())
+
+def clean_whitespace(name):
+    return " ".join(name.split())
+
+def nametrans(name):
+    curname = name.split(",")
+    if len(curname) < 2:
+        print "  Period?"
+        curname = name.split(".")
+    print curname
+    last, first = curname[0], curname[1]
+    current_name = first + " " + last
+    current_name = clean_whitespace(current_name)
+    return current_name
 
 def who_named_whom(filepath):
-#    os.chdir('../external/')
-#    print os.getcwd()
-#    ner_server_process = subprocess.Popen('./ner.sh')
- #   os.chdir('../testimony/')
-#    ner_server_process = subprocess.Popen('../external/ner.sh')
-    counter = 0
     namedict = {}
-    named_regex = re.compile("(^(\s)?[A-Z]\w+[,|\.]\s*[A-Z]\w+(?:\.)?([\s+]\w+)?)", 
+    named_regex = re.compile("(^(\s)?[A-Z]\w+[,|\.]\s*[A-Z]\w+(?:\s*[A-Z]\.)?([\s+]\w+)?)(\s*\((.*)\))?",
                              re.MULTILINE)
     not_all_caps_regex = re.compile("([a-z])")
     testimony_identifying_regex = re.compile("Testimony identifying")
@@ -31,7 +40,8 @@ def who_named_whom(filepath):
         # if we don't have a named line, we don't care.
         if not matches:
             continue
-        current_name = matches[0][0]
+        orig_name = matches[0][0]
+        current_name = nametrans(orig_name) # normal First Last format
 
         # # sometimes the regex confused locations for people
         # # the name is separated by a comma/period. NER works well for names
@@ -48,7 +58,13 @@ def who_named_whom(filepath):
 
         print "named: ", current_name
 
-        current_line = current_line.replace(current_name, "")
+        current_line = current_line.replace(orig_name, "")
+
+        # remove anything in parens (...) on same line as name.
+        # they are usually aliases.
+        reg = re.compile("\(.*\)")
+        current_line = re.sub(reg, "", current_line)
+        
         # get all lines from current_line until the next named name.
         # could skip i to be where j is, but we can save that for later.
         named_lines = ""
@@ -62,31 +78,34 @@ def who_named_whom(filepath):
                 break
             named_lines += lines[j]
 
-        named_lines = current_line + named_lines# add the rest of the first line
+        named_lines = current_line + named_lines # add the rest of the first line
+        
+
+        named_lines = named_lines.replace("\n", "")
+        print "named lines for ", current_name, "are:\n", named_lines
         got_result = None
         while got_result == None:
             try:
                 entities = tagger.get_entities(named_lines)
                 got_result = True
             except:
- #                os.kill(ner_server_process.pid, 0)
-#                ner_server_process.kill()
- #               time.sleep(0.5)
-#                os.chdir('../external/')
-#                print os.getcwd()
-#                ner_server_process = subprocess.Popen('./ner.sh')
-#                os.chdir('../testimony/')
-#                time.sleep(3)
                 pass
-            
-        counter += 1
-        print "Server calls: ", counter
+                
+        # get the names!
+        print entities
         if not entities.has_key('PERSON'):
             continue
         names = entities['PERSON']
-#        names = tagger.get_entities(named_lines)['PERSON']
+        names = map(clean_whitespace, names)
 
-        print "named lines for ", current_name, "are:\n", named_lines
+        # all real names are at least two tokens long.
+        names = filter(lambda n: len(n.split()) > 1, names)
+
+        # if this key doesn't have any real names associated with it, skip.
+        if not names:
+            continue
+
+
         print "names extracted: ", names
         print "----------------------------"
 
@@ -95,6 +114,33 @@ def who_named_whom(filepath):
         else:
             namedict[current_name] = names
     return namedict
+
+
+def name_distribution_with_tokens(names):
+    import itertools
+    dist = defaultdict(lambda: 0)
+    all_names = []
+    for k, v in names.items():
+        all_names.append(k)
+        
+        # flattening lists of strings
+        v1 = [([x] if isinstance(x,basestring) else x) for x in v]
+        v = list(itertools.chain(*v1))
+        all_names.extend(v)
+
+    for name in all_names:
+        dist[name.lower()] += 1
+
+    for name, val in dist.items():
+        dist[name] = float(val)/len(dist.keys())
+    return dist
+
+def most_likely_name(name, dist):
+    def close(n1, n2):
+        return namedist.fuzzy_substring(n1.lower(), n2.lower()) < 4
+    maybes = dict((k, v) for k, v in dist.items() if close(name, k))
+    best = max(maybes.items(), key=lambda p: p[1])
+    return best[0]
 
 def test_ner():
     a = range(1, 30)
